19ef84868a0764d22fc72cf5afd3a1eb75be6433
AravindhanSampath <aravindsampath009@gmail.com>
2021-08-25 00:59:32 -0700

    Task Modified with close browser

--- src/test/java/org/xpath/MakeMyTrip.java
+++ src/test/java/org/xpath/MakeMyTrip.java
@@ -1,9 +1,12 @@
 package org.xpath;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -28,5 +31,11 @@ public class MakeMyTrip {
 		driver.findElement(By.xpath("//a[text()='Search']")).click();
 		String journeyPrice = driver.findElement(By.xpath("(//p[contains(@class,'blackText')]//ancestor::div[@class='priceSection'])[1]")).getText();
 		System.out.println("Your Journey will be cost of " + journeyPrice);
+	}
+	
+	@AfterClass
+	private void closeBrowser() {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.quit();
 	}
 }
